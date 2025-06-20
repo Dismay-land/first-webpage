b1dd224ed687032779c9fcd379af9b37afc93108
Dima <dima@Dimas-MacBook-Pro.local>
2025-06-20 16:24:23 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import Button from '@mui/material/Button';
+import Input from '@mui/material/Input';
 
-function App() {
+
+const  App = () => {
+  const [inputSearch, setInputSearch] = useState('');
+  const [outputSerch, setOutputSearch] = useState('');
+  const name = 'Dima'
+
+  const handleButtonClick = () => {
+    setOutputSearch(inputSearch);
+  };
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          My new web
+        </div>
+
+        <div>
+          <Input 
+          value={inputSearch}
+          onChange={(e) => setInputSearch(e.target.value)}
+          placeholder="Type somethin'"
+          key='inputSearch' 
+          variant="contained" style={{
+            width: '300px',
+            marginTop: '20px',
+            height: '50px',
+            fontSize: '20px',
+            color: 'white',
+            backgroundColor: 'blue',
+          }}>type somthing</Input>
+        </div>
+
+        <div style={{
+          marginTop: '20px',
+        }}>
+
+        
+          <Button variant="contained" 
+            onClick={handleButtonClick}
+          >OK</Button>
+        </div>
+
+        <div style={{
+          marginTop: '40px'
+        }}>
+         you typed '{outputSerch}'  {' :)'}
+        </div>
+
+       
       </header>
     </div>
   );
