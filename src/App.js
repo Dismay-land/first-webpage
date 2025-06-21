0e016d0e93a4fc4c5744f514d2010d9e60501e73
Dismay-land <vladimirov.dmytro.s@gmail.com>
2025-06-21 13:14:55 -0600

    added analitcs search

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { Analytics } from "@vercel/analytics/react"
 
 
 const  App = () => {
+  <Analytics/>
   const [inputSearch, setInputSearch] = useState('');
   const [outputSerch, setOutputSearch] = useState('');
   const name = 'Dima'
